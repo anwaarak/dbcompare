7cbc563b5092151c89a4ab465cbf86c999a8e7f2
Anwar Ali Khan <connectakonline@gmail.com>
2021-07-07 21:13:35 -0400

    Data correction for mysql db

--- dbcompare-core/src/main/resources/data.sql
+++ dbcompare-core/src/main/resources/data.sql
@@ -1,18 +1,3 @@
-DROP TABLE IF EXISTS SOURCE;
-DROP TABLE IF EXISTS TARGET;
-
-CREATE TABLE SOURCE (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
-);
-
-CREATE TABLE TARGET (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL
-);
-
 INSERT INTO SOURCE (first_name, last_name, career) VALUES
   ('Jeff', 'Bezos', 'AMZN'),
   ('Bill', 'Gates', 'MSFT'),
@@ -21,4 +6,6 @@ INSERT INTO SOURCE (first_name, last_name, career) VALUES
 INSERT INTO TARGET (first_name) VALUES
   ('Jeff'),
   ('Bill'),
+  ('AK'),
   ('Elon');
+  
